81508eee78e77b967313e349f0be4e4ae0724d5d
Rizky Aldi Saputra <rizkyaldi0101@gmail.com>
2019-05-25 15:59:10 +0700

    bug login

---
+++ app/src/main/java/com/android/projectara/chatServices/Register.kt
@@ -0,0 +1,12 @@
+package com.android.projectara.chatServices
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.android.projectara.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
