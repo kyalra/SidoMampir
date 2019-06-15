1ebc7fa612e9d0b7b2235333fc8bcd2f1f1e6e44
ArmandoUtamaPutra <cronnosteam@gmail.com>
2019-06-15 16:49:04 +0700

    Update profile

--- app/src/main/java/com/android/projectara/MainActivity.kt
+++ app/src/main/java/com/android/projectara/MainActivity.kt
@@ -2,11 +2,22 @@ package com.android.projectara
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.android.projectara.modal.Post
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val posts: ArrayList<Post> = ArrayList()
+        for (i in 1..100) {
+            posts.add(Post("danielmalone_" + i, "asdf asdf text here", "https://picsum.photos/600/300?random&" + i))
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = PostAdapter(posts, this)
     }
 }
+
---
+++ app/src/main/java/com/android/projectara/modal/Post.kt
@@ -0,0 +1,3 @@
+package com.android.projectara.modal
+
+data class Post(val username: String, val text: String, val photo: String)
